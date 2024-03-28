a427502fb2c70126dd6ba6e7f8a469b12112169e
Udokyo <26225687-Udokyo@users.noreply.replit.com>
2024-03-28 11:02:00 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@DSA-project-demo/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include "Patient.h"
+
+// Function to create a new patient by asking the user for input
+Patient createPatientFromInput() {
+  std::string fname, lname, dob, phone, email;
+
+  std::cout << "Enter Patient First Name: ";
+  std::cin >> fname;
+  std::cout << "Enter Patient Last Name: ";
+  std::cin >> lname;
+  std::cout << "Enter Patient DOB (MM-DD-YYYY): ";
+  std::cin >> dob;
+  std::cout << "Enter Patient Phone Number: ";
+  std::cin >> phone;
+  std::cin >> phone;
+  std::cout << "Enter Patient Email: ";
+  std::cin >> email;
+
+  return Patient(fname, lname, dob, phone, email);
+}
+
+int main() {
+  int optionSelection;
+  // patient data type
+  // Patient patientID;
+  std::cout << "Welcome to our Dental Clinic\n" 
+    << "Select from the options below\n"
+    << "1. Patient Registeration\n"
+    << "2. Schedule an appointment\n"
+    << "3. View available Dentists\n"
+    << "4. View upcoming appointments\n"
+    << "5. View Patient information and medical history";
+  // implement try catch exception
+  std::cin >> optionSelection;
+  if (optionSelection == 1) {
+    std::cout << "Please enter the patient's information below:\n";
+    Patient newPatient = createPatientFromInput();
+    // Further processing with newPatient can be done here, like adding to patient records.
+  }
+  else if (optionSelection == 2) {
+
+  } 
+  return 0;
+}
---
+++ .ccls-cache/@home@runner@DSA-project-demo/patient.h
@@ -0,0 +1,40 @@
+#ifndef PATIENT_H
+#define PATIENT_H
+
+#include <string>
+#include <vector>
+
+class Patient {
+private:
+    static int nextID; // Static member to hold the next available ID;
+    int patientID; // Instance-specific ID
+    std::string firstName;
+    std::string lastName;
+    std::string dob; // Date of Birth
+    std::string phone;
+    std::string email;
+
+public:
+    Patient() = default;
+    Patient(const std::string& fname, const std::string& lname,
+        const std::string& dob, const std::string& phone, const std::string& email);
+
+
+    // Getters
+    static int generateNextID(); // Static function to update and return the next ID
+    int getPatientID() const;
+    std::string getFirstName() const;
+    std::string getLastName() const;
+    std::string getDOB() const;
+    std::string getPhone() const;
+    std::string getEmail() const;
+
+    // Setters
+    void setFirstName(const std::string& fname);
+    void setLastName(const std::string& lname);
+    void setDOB(const std::string& dob);
+    void setPhone(const std::string& phone);
+    void setEmail(const std::string& email);
+};
+
+#endif // PATIENT_H
